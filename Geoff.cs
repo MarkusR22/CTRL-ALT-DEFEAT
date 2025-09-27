fd96f72a993f7ddbaa4c0058e51c5eee55943459
MarkusR22 <markusreblin22@gmail.com>
2025-09-27 13:24:57 +0200

    Added tracking

--- Geoff.cs
+++ Geoff.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Drawing;
 using Robocode.TankRoyale.BotApi;
 using Robocode.TankRoyale.BotApi.Events;
@@ -29,6 +30,7 @@ public class Geoff : Bot
             TurnGunLeft(360);
             Back(100);
             TurnGunLeft(360);
+            TurnGunRight(10);
         }
     }
 
@@ -36,9 +38,22 @@ public class Geoff : Bot
     public override void OnScannedBot(ScannedBotEvent evt)
     {
 
-        
+        var bearingFromGun = GunBearingTo(evt.X, evt.Y);
 
-        Fire(1);
+        // Turn the gun toward the scanned bot
+        TurnGunLeft(bearingFromGun);
+
+        // If it is close enough, fire!
+        if (Math.Abs(bearingFromGun) <= 3 && GunHeat == 0)
+            Fire(Math.Min(3 - Math.Abs(bearingFromGun), Energy - .1));
+
+        // Generates another scan event if we see a bot.
+        // We only need to call this if the gun (and therefore radar)
+        // are not turning. Otherwise, scan is called automatically.
+        if (bearingFromGun == 0)
+            Rescan();
+
+        //Fire(1);
     }
 
     // We were hit by a bullet -> turn perpendicular to the bullet
